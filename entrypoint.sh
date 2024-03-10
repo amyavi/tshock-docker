5960d2994e3758f1a27962450237ed6699ba98e6
amyavi <144570677+amyavi@users.noreply.github.com>
2024-03-10 12:45:10 -0300

    feat: plugin support, extra params

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,12 @@ set -xe
 export DOTNET_BUNDLE_EXTRACT_BASE_DIR=/tmp/dotnet-bundle
 export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=0
 
+# shellcheck disable=SC2086
 /opt/tshock/TShock.Server \
     --stats-optout \
     -savedirectory /tmp/terraria-savedirectory \
+    -additionalplugins /data/plugins \
     -connperip "$TERRARIA_CONN_PER_IP" -killinactivesocket \
-    -autocreate "$TERRARIA_WORLD_SIZE" \
-    -world "$TERRARIA_WORLD_NAME".wld
+    -autocreate "$TERRARIA_WORLD_SIZE" -difficulty "$TERRARIA_WORLD_DIFFICULTY" \
+    -world "$TERRARIA_WORLD_NAME".wld \
+    $TERRARIA_EXTRA_PARAMS
