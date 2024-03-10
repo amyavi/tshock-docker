d6a6bf6b1f959af06a8596b5461ac7ed267f1b6e
amyavi <144570677+amyavi@users.noreply.github.com>
2024-03-10 13:31:54 -0300

    fix: fix plugin support

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,13 @@ set -xe
 export DOTNET_BUNDLE_EXTRACT_BASE_DIR=/tmp/dotnet-bundle
 export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=0
 
+cp --force --archive --update /data/plugins/bin/. /opt/tshock/bin
+cp --force --archive --update /data/plugins/*.dll /opt/tshock/ServerPlugins
+
 # shellcheck disable=SC2086
 /opt/tshock/TShock.Server \
     --stats-optout \
     -savedirectory /tmp/terraria-savedirectory \
-    -additionalplugins /data/plugins \
     -connperip "$TERRARIA_CONN_PER_IP" -killinactivesocket \
     -autocreate "$TERRARIA_WORLD_SIZE" -difficulty "$TERRARIA_WORLD_DIFFICULTY" \
     -world "$TERRARIA_WORLD_NAME".wld \
